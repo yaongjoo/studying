5fe6ae94187a574a521de1c57a1a7e393010f260
Yaongjoo <122340342+yaongjoo@users.noreply.github.com>
2025-01-24 19:41:45 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System; // == namespace 클래스들의 집합
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//C#: 닷넷 프레임워크 기반 범용 목적의 다중 패러다임 프로그래밍 언어
+//닷넷 프레임워크란 _ 프로그램을 만들기 위한 오픈소스, 크로스 플랫폼 개발 환경
+//C#은 클래스를 설명해야만 함
+
+// Class : 객체지향 프로그래밍의 가장 핵심적인 내용을 설명
+// 객체지향 : 내가 표현하고 싶은 걸 클래스로 만들어서
+// 그 클래스로 객체를 만드는 프로그래밍
+// 객체를 기반으로 모든 걸 해결
+// Class를 선언한다 -> 객체지향 프로그래밍을 한다
+
+//구현과 개념이 분리되지 않음
+
+//01.24 4강
+
+namespace C__Studying
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
