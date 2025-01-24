7a2ceee989954c9dd2d8d35fa819ea8305d34e0d
Yaongjoo <122340342+yaongjoo@users.noreply.github.com>
2025-01-24 19:44:33 +0900

    Add files via upload

---
+++ NameSpace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Class 식별자: 코드적 내용을 묶는 하나의 명칭
+
+//네임스페이스 : 개념의 분류
+
+class Potion
+{
+
+}
+
+namespace _02NameSpace
+{
+    internal class NameSpace
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
