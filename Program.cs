a5b40d918466c8be1e0aba4cb67a54b0b599b4ad
Yaongjoo <122340342+yaongjoo@users.noreply.github.com>
2025-02-03 15:42:23 +0900

    Add files via upload
    MemberVar

--- Program.cs
+++ Program.cs
@@ -4,16 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Class 식별자: 코드적 내용을 묶는 하나의 명칭
+// 한글을 보통 사용하지 않는다.
+// 쓸 수 없는 것은 X
+// 일단 모든 이러한 시스템은 영어 코드를 사용
+// 한글 처리 미흡
 
-//네임스페이스 : 개념의 분류
+// 게임을 만들게 되면
+// 큰 개념,
 
-class Potion
+// 플레이어가 가질 수 있는 세부적이 내용
+// Gold
+// 경험치
+// 공격력
+// 방어력
+// 명사적 개념
+// 대부분 멤버 변수로 표현
+
+// Status (큰 개념)
+// 
+
+
+
+class Player
 {
 
+    // int(자료형) 이름(식별자)
+    // 초기 값을 넣어줄 수 있다.
+    // 리터럴 값이라고 함
+    // 선언을 함과 동시에 값을 넣어주는 것 = 리터럴 초기화
+    
+    //내용 채우기
+    int Gold;
+    int Exp;
+    int Att;
+    int Def;
+    int Hp;
+ 
 }
 
-namespace _02NameSpace
+
+
+namespace _04MemberVar
 {
     internal class Program
     {
